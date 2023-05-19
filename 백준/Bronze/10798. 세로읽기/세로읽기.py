de9241efa8c9e5542827f56984bd48c498a92396
Jennie <92010971+yangjenniee@users.noreply.github.com>
2023-05-19 17:53:18 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,13 @@
+import sys
+
+words = [sys.stdin.readline().rstrip() for i in range(5)]
+
+for j in range(15) :
+    for i in range(5) :
+        if j<len(words[i]) :
+            print(words[i][j],end='')
+
+
+
+
+
