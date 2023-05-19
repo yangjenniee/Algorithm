7200854140ad18e538dbd0c7a362bf4f40e2783b
Jennie <92010971+yangjenniee@users.noreply.github.com>
2023-05-19 17:53:41 +0900

    [Bronze II] Title: 진법 변환, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -0,0 +1,13 @@
+import sys
+
+n,b = sys.stdin.readline().rstrip().split()
+n = ''.join(reversed(n))
+b = int(b)
+
+number = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+result = 0
+
+for x in range(len(n)-1,-1,-1) :
+    sum = number.index(n[x]) * (b**x)
+    result += sum
+print(result)
