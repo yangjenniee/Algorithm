3905dd019a0f306890d7f876f9bd12dd06b65ada
Jennie <92010971+yangjenniee@users.noreply.github.com>
2023-05-19 17:42:39 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,19 @@
+score=int(input())
+
+if(score>=0 and score<=100):
+    if(score>=90):
+        print("A")
+    elif(score>=80):
+        print("B")
+    elif(score>=70):
+        print("C")
+    elif(score>=60):
+        print("D")
+    else:
+        print("F")
+
+
+
+
+
+        
