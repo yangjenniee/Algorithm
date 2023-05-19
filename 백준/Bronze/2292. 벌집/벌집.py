25fc6442b09eb2e38efc2922578d2a899a1a109a
Jennie <92010971+yangjenniee@users.noreply.github.com>
2023-05-19 17:54:20 +0900

    [Bronze II] Title: 벌집, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -0,0 +1,13 @@
+import sys
+
+n=int(sys.stdin.readline().rstrip())
+nums_pileup = 1 #벌집의 개수, 1개부터 시작
+cnt = 1
+while n>nums_pileup :
+    nums_pileup += 6 *cnt #벌집이 6의 배수로 증가
+    cnt +=1 #반복문을 반복하는 횟수
+print(cnt)
+
+
+
+
